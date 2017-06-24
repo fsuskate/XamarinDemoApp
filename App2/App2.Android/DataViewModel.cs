c873f2624c369e46e73916b59841b5d04177a083
Francis Surroca <Francis_Surroca@symantec.com>
2017-06-24 13:09:39 -0400

    Initial commit

---
+++ App2/App2.Android/DataViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace App2
+{
+    public class DataViewModel : INotifyPropertyChanged
+    {
+        public DataViewModel()
+        {
+            UserName = "Test";
+            Password = "Pass";
+        }
+
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName]string name = "") =>
+           PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
---
+++ App2/App2/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var dataViewModel = this.BindingContext as DataViewModel;
+            string message = $"Welcome {dataViewModel.UserName} to Zombocom";
+            DisplayAlert("Login Successful", message, "Ok");
+        }
+
+    }
+}
